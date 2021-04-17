8ddd89acf02eed6077a1504e268f321df721fa04
Matt Ralston <matt@mralston.co.uk>
2021-04-17 02:18:41 +0100

    Initial commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace MRalston\LaravelAdditionalStringHelpers;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
+use League\CommonMark\GithubFlavoredMarkdownConverter;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        Str::macro('markdown', function ($content) {
+            $converter = new GithubFlavoredMarkdownConverter([
+                'html_input' => 'strip',
+                'allow_unsafe_links' => false,
+            ]);
+
+            return $converter->convertToHtml($content);
+        });
+
+        Str::macro('pluralPhrase', function ($value) {
+            $words = explode(' ', $value);
+
+            if (count($words) > 1) {
+                $lastWord = array_pop($words);
+
+                return implode(' ', $words).' '.self::plural($lastWord);
+            } else {
+                return self::plural($words[0]);
+            }
+        });
+
+        Stringable::macro('pluralPhrase', function () {
+            return new static(Str::pluralPhrase($this->value));
+        });
+    }
+}
